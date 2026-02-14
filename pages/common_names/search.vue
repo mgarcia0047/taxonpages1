8f822807a5c41d405a8d54192d7f4615b59829fa
mgarcia0047 <gmayrolin@gmail.com>
2026-02-13 22:44:26 -0500

    Update search.vue
    missing tag?

--- pages/common_names/search.vue
+++ pages/common_names/search.vue
@@ -5,7 +5,7 @@
       <div class="flex flex-col justify-center px-4">
         <!-- No results message -->
         <div v-if="!isLoading && list.length === 0" class="text-center">
-          <p class="text-lg">No scales found to be associated with the common name"{{ route.query.name }}"</p>
+          <p class="text-lg">No scales found to be associated with the common name "{{ route.query.name }}"</p>
         </div>
         
         <!-- Results list -->
