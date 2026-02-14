8a2241ef3a3e1e971345d585d20095cee4c97cd2
mgarcia0047 <gmayrolin@gmail.com>
2026-02-13 22:58:01 -0500

    Update search.vue
    changed route.query

--- pages/common_names/search.vue
+++ pages/common_names/search.vue
@@ -5,7 +5,7 @@
       <div class="flex flex-col justify-center px-4">
         <!-- No results message -->
         <div v-if="!isLoading && list.length === 0" class="text-center">
-          <p class="text-lg">No scales found to be associated with the common name "{{ route.query.name }}"</p>
+          <p class="text-lg">No scales found to be associated with the common name "{{ route.query.common_name }}"</p>
         </div>
         
         <!-- Results list -->
@@ -38,7 +38,7 @@ const list = ref([])
 
 onMounted(() => {
   const params = {
-    name: route.query.name,  // The search term from URL
+    name: route.query.common_name,  // The search term from URL
   }
   loadCommonNames(params)
 })
