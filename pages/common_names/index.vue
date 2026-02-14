c1be920868ef568b4f07b41656451900c817446e
mgarcia0047 <gmayrolin@gmail.com>
2026-02-13 20:56:28 -0500

    Create index.vue
    copy paste and edited index.vue from the author page

---
+++ pages/common_names/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="bg-base-foreground h-full">
+    <div class="mx-auto container py-12">
+      <div class="flex flex-col justify-center px-4">
+        <div class="w-auto mx-auto flex flex-col gap-4">
+          <img
+            :src="ScaleLogo"
+            class="mx-auto cursor-progress my-auto w-[400px]"
+          />
+
+          <div class="max-w-[400px] flex flex-row">
+            <input
+              class="scalenet-input"
+              type="text"
+              placeholder="Search by common name..."
+              v-model="common_name"
+              @keypress.enter="loadSearchPage"
+            />
+            <VButton
+              primary
+              @click="loadSearchPage"
+            >
+              Search
+            </VButton>
+          </div>
+          <div></div>
+          <div class="max-w-[400px] text-sm">
+            <p class="mb-2">
+              <b>Use:</b> Enter a common name, or a part of a common name, to find valid scientific names and relevant citations. For other types of queries, use the search links at the top of the page. Alternatively, you can search for catalogue entries by drilling down through the classification. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import ScaleLogo from '../images/SN-logo-400.png'
+
+const router = useRouter()
+const author = ref('')
+
+function loadSearchPage() {
+  router.push({
+    path: '/common_name/search',
+    query: {
+      common_name: common_name.value
+    }
+  })
+}
+</script>
